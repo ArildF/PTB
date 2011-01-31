837d3e8e3c6698a485890bcfec344fe8a4a0e89c
Arild <arild.fines@broadpark.no>
2011-01-31 23:36:12 +0100

    Replaced dragging codfe with an explicit behavior

---
+++ src/UI/Behaviors/DragGridElementBehavior.cs
@@ -0,0 +1,179 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+using System.Windows.Media;
+using Rogue.Ptb.Infrastructure;
+
+namespace Rogue.Ptb.UI.Behaviors
+{
+	public class DragGridElementBehavior : Behavior<Grid>
+	{
+		private Point _startPoint;
+		private UIElement _movedObject;
+		private Transform _originalTransform;
+
+		public static readonly DependencyProperty Draggable = DependencyProperty.RegisterAttached(
+			"DraggableProperty", typeof (bool), typeof (DragGridElementBehavior));
+
+		public static bool GetDraggable(DependencyObject d)
+		{
+			return (bool)d.GetValue(Draggable);
+		}
+
+		public static void SetDraggable(DependencyObject d, bool value)
+		{
+			d.SetValue(Draggable, value);
+		}
+
+		public DragGridElementBehavior()
+		{
+			AllElementsDraggable = true;
+		}
+
+		public bool AllElementsDraggable { get; set; }
+
+		protected override void OnAttached()
+		{
+			AssociatedObject.PreviewMouseLeftButtonDown += OnPreviewMouseLeftButtonDown;
+		}
+
+		protected override void OnDetaching()
+		{
+			AssociatedObject.PreviewMouseLeftButtonDown -= OnPreviewMouseLeftButtonDown;
+		}
+
+		private void OnPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			var gridRelativeStartPoint = e.GetPosition(AssociatedObject);
+
+			HitTestResult hitTestResult = null;
+			VisualTreeHelper.HitTest(AssociatedObject,
+				d => (AllElementsDraggable || GetDraggable(d)) ? HitTestFilterBehavior.ContinueSkipChildren : HitTestFilterBehavior.Continue,
+				htr =>
+				{
+					hitTestResult = htr;
+					return HitTestResultBehavior.Stop;
+				},
+					new PointHitTestParameters(gridRelativeStartPoint)
+				);
+
+			if (hitTestResult == null)
+			{
+				return;
+			}
+
+			_movedObject = hitTestResult.VisualHit as UIElement;
+
+			if (_movedObject == null || !(AllElementsDraggable || GetDraggable(_movedObject)))
+			{
+				return;
+			}
+
+			_startPoint = e.GetPosition(RootElement);
+			_originalTransform = _movedObject.RenderTransform;
+			AssociatedObject.CaptureMouse();
+			AssociatedObject.MouseMove += OnMouseMove;
+			AssociatedObject.MouseLeftButtonUp += OnMouseUp;
+
+			e.Handled = true;
+		}
+
+		protected UIElement RootElement
+		{
+			get
+			{
+				return AssociatedObject.TraverseBy<UIElement>(o => (UIElement) VisualTreeHelper.GetParent(o))
+					.Where(elt => elt != null).Last();
+			}
+		}
+
+		private void OnMouseMove(object sender, MouseEventArgs e)
+		{
+			var currentPoint = e.GetPosition(RootElement);
+
+			var translation = _originalTransform.Value;
+			translation.Translate(currentPoint.X - _startPoint.X, currentPoint.Y - _startPoint.Y);
+			
+			_movedObject.RenderTransform = new MatrixTransform(
+				translation);
+		}
+
+
+		private void OnMouseUp(object sender, MouseButtonEventArgs e)
+		{
+			AssociatedObject.MouseMove -= OnMouseMove;
+			AssociatedObject.MouseLeftButtonUp -= OnMouseUp;
+			AssociatedObject.ReleaseMouseCapture();
+
+			var position = e.GetPosition(AssociatedObject);
+			GridLocation location = FindGridLocation(position);
+			if (!location.IsEmpty)
+			{
+				Grid.SetColumn(_movedObject, location.X);
+				Grid.SetRow(_movedObject, location.Y);
+			}
+
+			_movedObject.RenderTransform = _originalTransform;
+		}
+
+		private GridLocation FindGridLocation(Point position)
+		{
+			if (position.X < 0 || position.X > AssociatedObject.ActualWidth ||
+				position.Y < 0 || position.Y > AssociatedObject.ActualHeight)
+
+			{
+				return GridLocation.MakeEmpty();
+			}
+
+			int x = 0, y = 0;
+			double columnRight = 0, rowTop = 0;
+			for (int i = 0; i < AssociatedObject.ColumnDefinitions.Count; i++)
+			{
+				var cd = AssociatedObject.ColumnDefinitions[i];
+
+				columnRight += cd.ActualWidth;
+				if (position.X <= columnRight)
+				{
+					x = i;
+					break;
+				}
+			}
+			for (int i = 0; i < AssociatedObject.RowDefinitions.Count; i++)
+			{
+				var cd = AssociatedObject.RowDefinitions[i];
+
+				rowTop += cd.ActualHeight;
+				if (position.Y <= rowTop)
+				{
+					y = i;
+					break;
+				}
+			}
+			return new GridLocation(x, y);
+		}
+
+		private struct GridLocation
+		{
+			public readonly int X;
+			public readonly int Y;
+
+			public GridLocation(int x, int y)
+			{
+				X = x;
+				Y = y;
+			}
+
+			public bool IsEmpty
+			{
+				get { return X < 0 || Y < 0; }
+			}
+
+			public static GridLocation MakeEmpty()
+			{
+				return new GridLocation(-1, -1);
+			}
+		}
+	}
+}
--- src/UI/Views/TaskBoardView.xaml.cs
+++ src/UI/Views/TaskBoardView.xaml.cs
@@ -1,18 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Interactivity;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Microsoft.Expression.Interactivity.Layout;
 using Rogue.Ptb.Infrastructure;
 
 namespace Rogue.Ptb.UI.Views
@@ -35,49 +26,6 @@ namespace Rogue.Ptb.UI.Views
 		public UIElement Element
 		{
 			get { return this; }
-		}
-
-		private void MouseDragElementBehaviorOnDragFinished(object sender, MouseEventArgs args)
-		{
-			var draggedElement = (UIElement)((IAttachedObject) sender).AssociatedObject;
-			var position = args.GetPosition(_itemsControl);
-
-			IInputElement elt= _itemsControl.InputHitTest(position);
-			var elt2 = elt as UIElement;
-
-			if (elt2 == null)
-			{
-				draggedElement.RenderTransform = Transform.Identity;
-				return;
-			}
-
-			var grid = elt2.TraverseBy(e => (UIElement) VisualTreeHelper.GetParent(e)).OfType<Grid>().FirstOrDefault();
-
-			if (grid == null)
-			{
-				return;
-			}
-
-
-			double total = 0;
-			position = args.GetPosition(grid);
-
-			for (int i = 0; i < grid.ColumnDefinitions.Count; i++)
-			{
-				var coldef = grid.ColumnDefinitions[i];
-				total += coldef.ActualWidth;
-
-				if (i.In(1, 3))
-				{
-					continue;
-				}
-				if (position.X < total)
-				{
-					Grid.SetColumn(draggedElement, i);
-					draggedElement.RenderTransform = Transform.Identity;
-					break;
-				}
-			}
 		}
 	}
 
