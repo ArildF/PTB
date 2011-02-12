c43dcfc1e7bb7d58332c1e6fcb3037d3f563a855
Arild <arild.fines@broadpark.no>
2011-02-12 11:40:00 +0100

    Select all text when editing existing task

---
+++ src/UI/Behaviors/SelectAllOnFocusBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace Rogue.Ptb.UI.Behaviors
+{
+	public class SelectAllOnFocusBehavior : Behavior<TextBoxBase>
+	{
+		protected override void OnAttached()
+		{
+			AssociatedObject.GotKeyboardFocus += AssociatedObjectOnGotKeyboardFocus;
+		}
+
+		protected override void OnDetaching()
+		{
+			AssociatedObject.GotKeyboardFocus -= AssociatedObjectOnGotKeyboardFocus;
+		}
+
+		private void AssociatedObjectOnGotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+		{
+			AssociatedObject.SelectAll();
+		}
+	}
+}
