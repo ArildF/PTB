e19eaad9e14f99b8f1d9417e75ce4146532dc11d
Arild <arild.fines@broadpark.no>
2011-02-28 20:28:06 +0100

    Show current task board in title bar

--- src/UI/ViewModels/ShellViewModel.cs
+++ src/UI/ViewModels/ShellViewModel.cs
@@ -1,15 +1,37 @@
 ﻿using System.Windows.Input;
+using ReactiveUI;
+using Rogue.Ptb.Core;
+using Rogue.Ptb.Infrastructure;
 using Rogue.Ptb.UI.Commands;
+using System;
 
 namespace Rogue.Ptb.UI.ViewModels
 {
-	public class ShellViewModel : IShellViewModel, ICommandResolver
+	public class ShellViewModel : ViewModelBase, IShellViewModel, ICommandResolver
 	{
 		private readonly ICommandResolver _resolver;
+		private readonly IEventAggregator _eventAggregator;
+		private string _title;
 
-		public ShellViewModel(ICommandResolver resolver)
+		public ShellViewModel(ICommandResolver resolver, IEventAggregator eventAggregator)
 		{
 			_resolver = resolver;
+			_eventAggregator = eventAggregator;
+
+			_eventAggregator.Listen<DatabaseChanged>().Subscribe(OnDatabaseChanged);
+
+			Title = "Personal Task Board - (no task board loaded)";
+		}
+
+		private void OnDatabaseChanged(DatabaseChanged evt)
+		{
+			Title = String.Format("Personal Task Board - {0}", evt.Path);
+		}
+
+		public string Title
+		{
+			get { return _title; }
+			set { this.RaiseAndSetIfChanged(vm => vm.Title, value); }
 		}
 
 		public ICommand Resolve(CommandName commandName)
