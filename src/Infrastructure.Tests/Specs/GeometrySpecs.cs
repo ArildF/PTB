99e9e91afc3c0cd81f9403b70ff67b355bfd75ff
Arild <arild.fines@broadpark.no>
2011-07-13 23:00:51 +0200

    Created a fluent API for making PathSegments

---
+++ src/Infrastructure.Tests/Specs/GeometrySpecs.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using Machine.Specifications;
+
+namespace Rogue.Ptb.Infrastructure.Tests.Specs
+{
+	[Subject(typeof (BuildPath))]
+	public class When_constructing_a_path_geometry_through_the_fluent_API
+	{
+		Establish context = () =>
+			{
+				builder = BuildPath.From(0, 0).To(10, 10).CurveTo(15, 15, SweepDirection.Clockwise);
+			};
+
+
+
+		Because of = () => { geometry = builder.Build(); };
+
+		It should_have_one_figure = () => geometry.Figures.Count.ShouldEqual(1);
+
+		It should_start_at_zero_zero = () => geometry.Figures[0].StartPoint.ShouldEqual(new Point(0, 0));
+
+		It should_have_two_segments = () => geometry.Figures[0].Segments.Count.ShouldEqual(2);
+
+		It should_have_first_a_line_segment = () => geometry.Figures[0].Segments[0].ShouldBe(typeof (LineSegment));
+
+		It should_have_a_line_segment_to_ten_ten = () => ((LineSegment)geometry.Figures[0].Segments[0]).Point.ShouldEqual(new Point(10, 10));
+
+		It should_have_second_an_arc_segment = () => geometry.Figures[0].Segments[1].ShouldBe(typeof(ArcSegment));
+
+		It should_have_an_arc_segment_to_15_15 = () => ((ArcSegment)geometry.Figures[0].Segments[1]).Point.ShouldEqual(new Point(15, 15));
+
+
+		private static PathGeometry geometry;
+		private static BuildPath.IPathBuilder builder;
+	}
+}
---
+++ src/Infrastructure/BuildPath.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Rogue.Ptb.Infrastructure
+{
+	public class BuildPath
+	{
+		public interface IPathBuilder
+		{
+			IPathBuilder To(double x, double y);
+			IPathBuilder CurveTo(double x, double y, SweepDirection direction);
+			PathGeometry Build();
+		}
+
+		public static IPathBuilder From(double x, double y)
+		{
+			return new PathBuilder(x, y);
+		}
+
+		private class PathBuilder : IPathBuilder
+		{
+			private readonly Point _startPoint;
+			private readonly List<PathSegment> _segments = new List<PathSegment>();
+			private bool _currentIsStroked;
+
+			private Point _currentPoint;
+
+			public PathBuilder(double x, double y)
+			{
+				_currentPoint = _startPoint = new Point(x, y);
+				_currentIsStroked = true;
+			}
+
+			public IPathBuilder To(double x, double y)
+			{
+				_currentPoint = new Point(x, y);
+				_segments.Add(new LineSegment(_currentPoint, _currentIsStroked));
+				return this;
+			}
+
+			public IPathBuilder CurveTo(double x, double y, SweepDirection direction)
+			{
+				var newPoint = new Point(x, y);
+				var diff = Point.Subtract(_currentPoint, newPoint);
+				var radius = new Size(Math.Abs(diff.X), Math.Abs(diff.Y));
+
+				_segments.Add(new ArcSegment(new Point(x, y), radius, 45, false, direction, _currentIsStroked));
+				return this;
+			}
+
+			public PathGeometry Build()
+			{
+				var figures = new[]
+					{
+						new PathFigure(_startPoint, _segments, false),
+					};
+				return new PathGeometry(figures);
+			}
+		}
+	}
+}
