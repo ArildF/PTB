3596304a6bef7f27a9a877890669dfa9d280f409
Arild Fines <arild.fines@broadpark.no>
2021-10-07 00:21:50 +0200

    Make it possible to copy images from notes.

--- src/UI/Views/ImageWindow.xaml.cs
+++ src/UI/Views/ImageWindow.xaml.cs
@@ -1,13 +1,28 @@
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
 
 namespace Rogue.Ptb.UI.Views
 {
 	public partial class ImageWindow
 	{
+		private readonly Image _image;
+
 		public ImageWindow(Image image)
 		{
+			_image = image;
 			InitializeComponent();
 			Image.Source = image.Source;
+		}
+
+		private void CommandBinding_OnExecuted(object sender, ExecutedRoutedEventArgs e)
+		{
+			if (Image.Source is BitmapSource bs)
+			{
+				Clipboard.SetImage(bs);
+			}
+
 		}
 	}
 }
